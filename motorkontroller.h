1b01bea8ef7fe339a4a2f45778a31eac4e44a8d1
hegnaea <112164545+hegnaea@users.noreply.github.com>
2024-04-22 13:47:03 +0200

    Delete motorkontroller.h

--- motorkontroller.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOTORCONTROLLER_H
-#define MOTORCONTROLLER_H
-#include <stdbool.h>
-// Function prototypes
-int control_motors(int angle, int speed, bool joystick_nr);
-int mapValue(int value, int direction);
-
-#endif
