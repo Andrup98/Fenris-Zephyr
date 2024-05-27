ba777ae0407b29bd45ab54de8938040dca5b2966
Andrup98 <149668234+Andrup98@users.noreply.github.com>
2024-05-27 16:08:50 +0200

    Delete Motor_controller directory

--- Motor_controller/src/can_send.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAN_SEND_H
-#define CAN_SEND_H
-
-#define MSG_ID 0x0000002 // Message ID
-
-const char* can_state_to_str(enum can_state state);
-void send_string(const struct device *can_dev, const char *str);
-
-#endif // CAN_UTILS_H
