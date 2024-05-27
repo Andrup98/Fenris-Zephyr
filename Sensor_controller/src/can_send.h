458cecada670be8395a343e7ce0e60c436a0300d
Andrup98 <149668234+Andrup98@users.noreply.github.com>
2024-05-27 16:08:59 +0200

    Delete Sensor_controller directory

--- Sensor_controller/src/can_send.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAN_SEND_H
-#define CAN_SEND_H
-
-#define MSG_ID 0x0000003 // Message ID
-
-const char* can_state_to_str(enum can_state state);
-void send_byte(const struct device *can_dev, uint8_t sensor_byte);
-
-#endif // CAN_UTILS_H
