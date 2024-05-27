31df5e90655e8454b06c2a452f929a7bdc7178b6
Andrup98 <149668234+Andrup98@users.noreply.github.com>
2024-05-27 16:09:16 +0200

    Delete CANBUS_comm directory

--- CANBUS_comm/src/can_dev.h
+++
@@ -1,6 +0,0 @@
-#ifndef CAN_DEV_H
-#define CAN_DEV_H
-
-const struct device *const get_can_dev(void);
-
-#endif
--- CANBUS_comm/src/can_send.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAN_SEND_H
-#define CAN_SEND_H
-
-#define MSG_ID 0x0000001 // Message ID
-
-const char* can_state_to_str(enum can_state state);
-void send_custom_data (const struct device *can_dev, uint8_t js_bool, int16_t js_deg_int, int16_t js_spd_int);
-
-#endif // CAN_UTILS_H
